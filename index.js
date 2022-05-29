52c4c6da3595cebc70198328637cdafba298b4e7
ALPHA-704 <100064063+ALPHA-704@users.noreply.github.com>
2022-05-29 08:07:58 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+			 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<title>Page title</title>
+
+				<link rel="stylesheet" href="css/my.css">
+</head>
+
+<body>
+ <div class="text">
+   <h1 style=color:red;>welcome to my website</h1>
+ </div>
+ 
+ <div class="links">
+   
+ <h4><a href="https://TSHEPHANG-V/VOLTRON" class="button">BOT LINK</a></h4>
+ 
+  <h4><a href="https://replit.com/@TshepisoMasia/VOLTRON?v=1" class="button2">QR SITE</a></h4>
+  
+  <h4><a href="https://wa.me/27634923280?text=hi+bro+how+are+you+?+🖤" class="button3">contact owner</a></h4>
+  </div>
+<div class="copyright">
+  &copy; 2022 My website. All rights reserved
+</div>
+</body>
+</html>
